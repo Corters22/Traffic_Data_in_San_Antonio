ad7c20e4efa4f804e82078c8b86d3e7cfb0e85fa
Bobby Del Bosque <bobbydelbosque@outlook.ocm>
2021-02-13 08:15:24 -0600

    inital setup

---
+++ Question_2/__init__.py
@@ -0,0 +1,9 @@
+#API key: 
+#WeatherMap - Your b563c90d06045ec4e0c1e7bcbae79d8f
+weather_api_key = b563c90d06045ec4e0c1e7bcbae79d8f
+
+#google
+g_key = AIzaSyCYM_qX9ByMBKyjnMtAhK8iSDbuCJDCsjw
+
+#Census Data API. Your 
+api_key = 5a65af9041392cdd56f640cb47c2ac2fb05c8f4d
